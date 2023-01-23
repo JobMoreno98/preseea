757230af8ff4e2762d8c88d147cf5b6b148fc02e
Job Moreno <jobmoreno98@gmail.com>
2023-01-22 18:44:23 -0600

    aviso de privasidad

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -146,10 +146,14 @@
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="text-center">
-                                    <p>Diseñado por <a href="http://www.cucsh.udg.mx" rel="nofollow"
-                                            target="_blank">CTA CUCSH</a>
-                                        <a href="https://transparencia.udg.mx/aviso-confidencialidad-integral" class="text-decoration-none text-white" target="_blank">Aviso de privasidad *</a>
-                                        </p>
+                                    <p>Diseñado por <a href="http://www.cucsh.udg.mx" rel="nofollow" target="_blank">CTA
+                                            CUCSH</a> 
+                                            | 
+                                        <a type="button" class="text-decoration-none text-white" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal">
+                                            Aviso de Privacidad
+                                        </a>
+                                    </p>
 
 
                                 </div>
@@ -161,7 +165,7 @@
     </div>
 
     <!--====== FOOTER PART ENDS ======-->
-    <!--====== Modal ======-->
+    <!--====== Modal Registro ======-->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog  modal-xl">
@@ -200,6 +204,49 @@
                 </div>
                 <div class="modal-footer">
                     <p class="text-muted">NOTA: los campos con (*) son obligatorios</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Button trigger modal -->
+
+
+    <!--====== Modal Aviso  ======-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">AVISO DE PRIVACIDAD</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <p class="text-justify">
+                        La Universidad de Guadalajara (en adelante UdeG), con domicilio en Avenida Juárez 976, colonia
+                        Centro, código postal 44100, en Guadalajara, Jalisco, hace de su conocimiento que se considerará
+                        como información confidencial aquella que se encuentre contemplada en los artículos 3, fracciones IX
+                        y X de la LPDPPSOEJM; 21 de la LTAIPEJM; Lineamientos Cuadragésimo Octavo y Cuadragésimo Noveno de
+                        los Lineamientos de Clasificación; Lineamientos Décimo Sexto, Décimo Séptimo y Quincuagésimo Octavo
+                        de los Lineamientos de Protección, así como aquellos datos de una persona física identificada o
+                        identificable y la inherente a las personas jurídicas, los cuales podrán ser sometidos a tratamiento
+                        y serán única y exclusivamente utilizados para los fines que fueron proporcionados, de acuerdo con
+                        las finalidades y atribuciones establecidas en los artículos 1, 5 y 6 de la Ley Orgánica, así como 2
+                        y 3 del Estatuto General, ambas legislaciones de la UdeG, de igual forma, para la prestación de los
+                        servicios que la misma ofrece conforme a las facultades y prerrogativas de la entidad universitaria
+                        correspondiente y estarán a resguardo y protección de la misma.
+                    </p>
+                    <p class="text-justify">
+                        Usted puede consultar nuestro Aviso de Privacidad integral en la siguiente <a
+                            href="http://www.transparencia.udg.mx/aviso-confidencialidad-integral" target="_blank"> página
+                            web</a> 
+                    </p>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                 </div>
             </div>
         </div>
